4a75defbdf29af8e2db36057ddb2dd4243be938d
Ziqi Wang <qiqiustc@gmail.com>
2019-10-30 10:43:44 +0100

    update

--- TopoRhinoCommand.cs
+++ TopoRhinoCommand.cs
@@ -36,56 +36,54 @@ namespace TopoRhino
             Guid current_layer_guid = doc.Layers.CurrentLayer.Id;
 
             double tile_angle = 0;
-            Rhino.Input.RhinoGet.GetNumber("Frame", false, ref tile_angle);
-
-            //for (double tile_angle = 0; tile_angle < 40; tile_angle += 40.0 / (float)number_frame)
-            //{ 
+            Rhino.Input.RhinoGet.GetNumber("tiltAngle", false, ref tile_angle, 0, 90);
             TopoCreator.setParaDouble("tiltAngle", tile_angle);
-                TopoCreator.refresh();
-                int n_part = TopoCreator.partNumber();
 
-                //Layer
-                Rhino.DocObjects.Layer childlayer = new Rhino.DocObjects.Layer();
-                childlayer.ParentLayerId = current_layer_guid;
-                childlayer.Name = String.Format("Tilt {0}", tile_angle);
-                int childindex = doc.Layers.Add(childlayer);
-                doc.Layers.SetCurrentLayerIndex(childindex, true);
+            TopoCreator.refresh();
+            int n_part = TopoCreator.partNumber();
 
-                for (int partID = 0; partID < n_part; partID++)
+            //Layer
+            Rhino.DocObjects.Layer childlayer = new Rhino.DocObjects.Layer();
+            childlayer.ParentLayerId = current_layer_guid;
+            childlayer.Name = String.Format("Tilt {0}", tile_angle);
+            int childindex = doc.Layers.Add(childlayer);
+            doc.Layers.SetCurrentLayerIndex(childindex, true);
+
+            for (int partID = 0; partID < n_part; partID++)
+            {
+                Rhino.Geometry.Mesh mesh = new Rhino.Geometry.Mesh();
+                TopoCreator.getMesh(partID, mesh);
+
+                Guid mesh_guid = doc.Objects.AddMesh(mesh);
+
+                //error: exit
+                if (mesh_guid == Guid.Empty)
                 {
-                    Rhino.Geometry.Mesh mesh = new Rhino.Geometry.Mesh();
-                    TopoCreator.getMesh(partID, mesh);
-
-                    Guid mesh_guid = doc.Objects.AddMesh(mesh);
-
-                    //error: exit
-                    if (mesh_guid == Guid.Empty)
-                    {
-                        TopoCreator.deleteStructure();
-                        return Result.Failure;
-                    }
-
-                    int mat_index;
-                    if (TopoCreator.isBoundary(partID))
-                    {
-                        mat_index = doc.Materials.Find("PlasterBoundary", false);
-                        //RhinoApp.WriteLine("{0}, {1}", partID, "PlasterBoundary");
-                    }
-                    else
-                    {
-                        mat_index = doc.Materials.Find("PlasterPart", false);
-                        //RhinoApp.WriteLine("{0}, {1}", partID, "PlasterPart");
-                    }
-
-                    if (mat_index != -1)
-                    {
-                        Rhino.DocObjects.RhinoObject obj = doc.Objects.Find(mesh_guid);
-                        obj.Attributes.LayerIndex = childindex;
-                        obj.Attributes.MaterialIndex = mat_index;
-                        obj.Attributes.MaterialSource = Rhino.DocObjects.ObjectMaterialSource.MaterialFromObject;
-                        obj.CommitChanges();
-                    }
+                    TopoCreator.deleteStructure();
+                    return Result.Failure;
                 }
+
+                int mat_index;
+                if (TopoCreator.isBoundary(partID))
+                {
+                    mat_index = doc.Materials.Find("PlasterBoundary", false);
+                    //RhinoApp.WriteLine("{0}, {1}", partID, "PlasterBoundary");
+                }
+                else
+                {
+                    mat_index = doc.Materials.Find("PlasterPart", false);
+                    //RhinoApp.WriteLine("{0}, {1}", partID, "PlasterPart");
+                }
+
+                if (mat_index != -1)
+                {
+                    Rhino.DocObjects.RhinoObject obj = doc.Objects.Find(mesh_guid);
+                    obj.Attributes.LayerIndex = childindex;
+                    obj.Attributes.MaterialIndex = mat_index;
+                    obj.Attributes.MaterialSource = Rhino.DocObjects.ObjectMaterialSource.MaterialFromObject;
+                    obj.CommitChanges();
+                }
+            }
             //}
 
             Rhino.RhinoApp.RunScript("_SelMesh", false);
